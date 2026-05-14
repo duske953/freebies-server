10b7a77e808059c64ebb253b732125976c7f0c74
Eloho Kennedy <kennyduske@gmail.com>
2026-05-15 00:24:54 +0100

    add rate limiter

--- app.ts
+++ app.ts
@@ -2,9 +2,17 @@ import express from 'express';
 import sslRouter from './api/ssl-checker/controller.ts';
 import analyzeRouter from './api/anaylze-site/controller.ts';
 import cors from 'cors';
+import limiter from './utils/rateLimiter.ts';
 const app = express();
+
+// rate limiter
+app.use(limiter);
+
+// body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// cors
 app.use(
   cors({
     origin: '*',
@@ -12,7 +20,9 @@ app.use(
   }),
 );
 
+// routes
 app.use('/check-ssl', sslRouter);
 app.use('/analyze-site', analyzeRouter);
 
+// server
 export default app;
